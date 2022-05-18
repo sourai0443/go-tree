a08de9928b6de62a40d74067d490745f660eaa46
casio0128-dev <sweet.cafe0141@gmail.com>
2022-05-18 20:30:48 +0900

    基本機能を実装

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"io/fs"
@@ -17,36 +18,63 @@ func main() {
 // 1 => │　+ ┝、or │ or └
 
 func getDirNames(root string, skipFunc func(entry os.DirEntry) bool, out io.Writer) {
+	// 現在処理中のファイル数
 	filesMap := make(map[string]int)
-	var desCnt int
-	filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+	// rootに指定したファイルが+1されるため打消し
+	filesMap[root] = -1
+	// ディレクトリ内のファイル数（ディレクトリを含む）
+	dirFiles := make(map[string]int)
+	// 空白列を記録するマップ
+	blankMap := make(map[int]interface{})
+
+	filepath.WalkDir(root, func(orgPath string, d fs.DirEntry, err error) error {
 		if skipFunc != nil && skipFunc(d) {
 			return nil
 		}
 
+		path := filepath.Clean(orgPath)
 		p, _ := filepath.Split(path)
+		p = filepath.Clean(p)
+
+		// 処理中のファイルがディレクトリの場合の処理
+		// ディレクトリ内のファイル数を記録する（└を打つため）
 		if d.IsDir() {
-			filesMap[p] = 0
 			des, err := os.ReadDir(path)
 			if err != nil {
 				return err
 			}
-			desCnt = len(des)
+			dirFiles[path] = len(des)
 		}
+
+		// 処理中のファイルが何番目かをインクリメント
 		filesMap[p]++
-		fmt.Println(desCnt, filesMap[p], d.Name(), p)
-
+		// インデントの深さを取得
 		cnt := strings.Count(path, string(os.PathSeparator))
+
+		buf := &bytes.Buffer{}
+		// 空白に指定されていなければ、│を出力し空白の場合は空文字列を出力
 		for i := 0; i < cnt; i++ {
-			fmt.Fprint(out, "│")
+			if _, ok := blankMap[i]; ok {
+				fmt.Fprint(buf, " ")
+			} else {
+				fmt.Fprint(buf, "│")
+			}
 		}
 
-		if desCnt == filesMap[p] {
-			fmt.Fprintf(out, "└%s\n", d.Name())
-			desCnt = 0
+		if dirFiles[p] == filesMap[p] {
+			// ディレクトリのファイル数と処理中のファイル数が一致した場合は└を出力。同じ列を空白に指定するマップに追加
+			fmt.Fprintf(buf, "└%s\n", d.Name())
+			blankMap[cnt] = struct{}{}
+		} else if strings.EqualFold(root, orgPath) {
+			// rootと同じファイルは罫線を着けずに出力
+			fmt.Fprintln(buf, root)
 		} else {
-			fmt.Fprintf(out, "┝%s\n", d.Name())
+			// ファイルの終端でも、rootファイルでもない場合は├を出力し、同じ列を空白に指定するマップから指定列数を削除
+			fmt.Fprintf(buf, "┝%s\n", d.Name())
+			delete(blankMap, cnt)
 		}
+		// 文字列として出力
+		fmt.Fprint(out, buf.String())
 		return nil
 	})
 }
