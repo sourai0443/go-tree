3158dce2af15d6bb682e4bc09159b52ea2817c14
casio0128-dev <sweet.cafe0141@gmail.com>
2022-05-19 20:52:37 +0900

    リファクタとツリー間のスペース表示のフラグを追加

--- main.go
+++ main.go
@@ -15,13 +15,15 @@ import (
 var (
 	target          string
 	out             string
+	spaces          uint
 	isDirectoryOnly bool
 )
 
 func init() {
-	kingpin.Flag("target", "").Short('t').Default(".").StringVar(&target)
-	kingpin.Flag("out", "").Short('o').Default("").StringVar(&out)
-	kingpin.Flag("dir-only", "").Short('d').Default("false").BoolVar(&isDirectoryOnly)
+	kingpin.Flag("target", "Specify the directory where you want the tree to appear.").Short('t').Default(".").StringVar(&target)
+	kingpin.Flag("out", "Specifies the output destination. Default is standard output.").Short('o').Default("").StringVar(&out)
+	kingpin.Flag("space", "Specifies the space between trees. Default is 0.").Short('s').Default("0").UintVar(&spaces)
+	kingpin.Flag("dir-only", "Only the directory structure is retrieved.").Short('d').Default("false").BoolVar(&isDirectoryOnly)
 	kingpin.Parse()
 }
 
@@ -30,6 +32,7 @@ const (
 	timeStampPattern      = "2006/01/02 15:04:05"
 	blank                 = ""
 	space                 = " "
+	horizontalBorder      = "─"
 	verticalBorder        = "│"
 	rightExistBorder      = "├"
 	endBorder             = "└"
@@ -95,16 +98,17 @@ func getDirNames(root string, out io.Writer, skipFunc func(entry os.DirEntry) bo
 		// 空白に指定されていなければ、│を出力し空白の場合は空文字列を出力
 		printVerticalBorder(buf, cnt, blankMap)
 
+		horizontalBorders := printHorizontalBorder(int(spaces))
 		if dirFiles[p] == filesMap[p] {
 			// ディレクトリのファイル数と処理中のファイル数が一致した場合は└を出力。同じ列を空白に指定するマップに追加
-			fmt.Fprintln(buf, endBorder+d.Name())
+			fmt.Fprintln(buf, endBorder+horizontalBorders+d.Name())
 			blankMap[cnt] = struct{}{}
 		} else if strings.EqualFold(root, orgPath) {
 			// rootと同じファイルは罫線を着けずに出力
 			fmt.Fprintln(buf, root)
 		} else {
 			// ファイルの終端でも、rootファイルでもない場合は├を出力し、同じ列を空白に指定するマップから指定列数を削除
-			fmt.Fprintln(buf, rightExistBorder+d.Name())
+			fmt.Fprintln(buf, rightExistBorder+horizontalBorders+d.Name())
 			delete(blankMap, cnt)
 		}
 		// 文字列として出力
@@ -137,9 +141,25 @@ func getFileCount(path string, isDirectoryOnly bool) (int, error) {
 func printVerticalBorder(out io.Writer, max int, blankMap map[int]interface{}) {
 	for i := 0; i < max; i++ {
 		if _, ok := blankMap[i]; ok {
-			fmt.Fprint(out, " ")
+			fmt.Fprint(out, printSpaces(int(spaces)+1))
 		} else {
-			fmt.Fprint(out, "│")
+			fmt.Fprint(out, verticalBorder+printSpaces(int(spaces)))
 		}
 	}
+}
+
+func printHorizontalBorder(cnt int) string {
+	var result string
+	for i := 0; i < cnt; i++ {
+		result += horizontalBorder
+	}
+	return result
+}
+
+func printSpaces(cnt int) string {
+	var result string
+	for i := 0; i < cnt; i++ {
+		result += space
+	}
+	return result
 }
